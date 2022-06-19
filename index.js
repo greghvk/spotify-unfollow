d59df724fcf8429217389675a1890cc0eb87e713
Grzegorz Huk <hook@Grzegorzs-MacBook-Air.local>
2022-06-19 17:21:23 +0200

    move html to root

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 1234;
-app.listen(port, () => console.log(`serving at port ${port}`));
 app.use(express.static("public"));
+
+app.use("/public", express.static(__dirname + "/public"))
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+})
+app.listen(port, () => console.log(`serving at port ${port}`));
+
